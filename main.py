26b9271fc11d9397698b657f83fee1021dc2c628
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-30 12:31:13 -0700

    feat: adding a fileupload endpoint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Response, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Response
 from fastapi.responses import JSONResponse
+import shutil
+from pathlib import Path
 import time
 
 app = FastAPI()
@@ -11,3 +13,14 @@ def delay_response(ms: int):
     
     time.sleep(ms / 1000)
     return JSONResponse(content={"status": "success", "time_slept_ms": ms }, status_code=200)
+
+@app.post("/v1/uploadfile/")
+async def upload_file(file: UploadFile = File(...)):
+    file_location = f"uploaded_files/{file.filename}"
+    with open(file_location, "wb+") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        
+    return {
+        "info": "success",
+        "filename": file.filename
+    }
